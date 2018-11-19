199a5dd2291529fc23f3b0e369f6dd9950647d76
jhtobav <jhtobav@gmail.com>
2018-11-19 16:19:20 +0000

    fixing error on dashboard template

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from . import models
 
 def dashboard(request):
-    context = {'message': "saludos desde dashboard"}
+    boards = models.Board.objects.all()
+    columns = models.Column.objects.all()
+    tasks = models.Task.objects.all()
+
+    context = {
+            'dashboards': boards,
+            'columns': columns,
+            'tasks': tasks,
+            }
     return render(request, 'dashboard/dashboard.html', context)
