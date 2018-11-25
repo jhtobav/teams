bc064ddd060c036bdf9ed2e3cfe05b5565a8fe0a
jhtobav <jhtobav@gmail.com>
2018-11-25 16:41:27 +0000

    Creating str methods to models on teamsapp

--- teamsapp/models.py
+++ teamsapp/models.py
@@ -6,8 +6,14 @@ class Team(models.Model):
     name = models.CharField(max_length=50)
     creation_date = models.DateField(default=timezone.now)
 
+    def __str__(self):
+        return self.name
+
 
 class Member(models.Model):
     team = models.ForeignKey(Team, on_delete='cascade')
     user_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{}-{}".format(self.team.name, self.user_id)
 
