c74b4a7e225f4aa2498d87fd0e42d2daafd92592
jhtobav <jhtobav@gmail.com>
2018-12-01 05:10:38 +0000

    Dashboard: Message framework and redirects implemented

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 # dashboard/views.py
-from django.shortcuts import render, Http404
+from django.shortcuts import redirect, render
+from django.http import Http404
+from django.contrib import messages
 from django.utils import timezone
 
 from .models import Board
@@ -29,18 +31,12 @@ def create_board(request, team_name):
     if request.method == 'POST':
         selected_team = Team.objects.get(name=team_name)
         board_name = request.POST.get('new_board_name', None)
-        slug = board_name
-        
+        slug = slugify(board_name)
         selected_team.board_set.create(name=board_name, slug=slug)
-        boards = selected_team.board_set.all()
+
         message = "Board created successfully"
-        context = {
-                'team': selected_team,
-                'boards': boards,
-                'message': message
-                }
-        
-        return render(request, 'dashboard/dashboard.html', context)
+        messages.add_message(request, messages.INFO, message)
+        return redirect('dashboard:dashboard.html', team_name=team_name)
     else:
         return Http404('Not allowed')
 
@@ -72,26 +68,14 @@ def delete_board(request, team_name, board_name):
         selected_board = selected_team.board_set.get(name=board_name)
         if selected_board.column_set.count() < 1:
             selected_board.delete()
-            boards = Board.objects.all()
-            message = "Board deleted successfully"
-            context = {
-                    'message': message,
-                    'team': selected_team,
-                    'boards': boards
-                    }
 
-            return render(request, 'dashboard/dashboard.html', context) 
+            message = "Board deleted successfully"
+            messages.add_message(request, messages.INFO, message)
+            return redirect('dashboard:dashboard', team_name=team_name)
         else:
             message = "The board still has columns, it can not be deleted"
-            columns =  selected_board.column_set.order_by('index')
-
-            context = {
-                    'message': message,
-                    'team': selected_team,
-                    'board': selected_board,
-                    'columns': columns
-                    }
-            return render(request, 'dashboard/board.html', context)
+            messages.add_message(request, messages.INFO, message)
+            return redirect('dashboard:board', team_name=team_name, board_name=board_name)
     else:
         return Http404('Not Allowed')
 
@@ -107,14 +91,9 @@ def create_column(request, team_name, board_name):
         column_index = selected_board.column_set.count()
         selected_board.column_set.create(name=column_name, index=column_index)
 
-        columns = selected_board.column_set.order_by('index')
-
-        context = {
-                'team': selected_team,
-                'board': selected_board,
-                'columns': columns
-                }
-        return render(request, 'dashboard/board.html', context)
+        message = "Column created successfully"
+        messages.add_message(request, messages.INFO, message)
+        return redirect('dashboard:board', team_name=team_name, board_name=board_name)
     else:
         return Http404('Not allowed')
 
@@ -130,24 +109,13 @@ def delete_column(request, team_name, board_name):
         if selected_column.task_set.count() < 1:
             selected_column.delete()
 
-            columns = selected_board.column_set.order_by('index')
-            context = {
-                    'team': selected_team,
-                    'board': selected_board,
-                    'columns': columns
-                    }
-            return render(request, 'dashboard/board.html', context)
+            message = "Column deleted successfully"
+            messages.add_message(request, messages.INFO, message)
+            return redirect('dashboard:board', team_name=team_name, board_name=board_name)
         else:
             message = "The column still has tasks, it can not be deleted"
-            columns = selected_board.column_set.order_by('index')
-
-            context = {
-                    'team': selected_team,
-                    'board': selected_board,
-                    'message': message,
-                    'columns': columns
-                    }
-            return render(request, 'dashboard/board.html', context)
+            messages.add_message(request, messages.INFO, message)
+            return redirect('dashboard:board', team_name=team_name, board_name=board_name)
     else:
         return Http404('Not allowed')
 
@@ -162,17 +130,11 @@ def create_task(request, team_name, board_name):
         selected_column = selected_board.column_set.get(name=request.POST.get('column_name', None))
         task_title = request.POST.get('title', None)
         task_description = request.POST.get('description', None)
-        
         selected_column.task_set.create(title=task_title, description=task_description, creation_date = timezone.now())
 
-        columns = selected_board.column_set.order_by('index')
-        
-        context = {
-                'team': selected_team,
-                'board': selected_board,
-                'columns': columns
-                }
-        return render(request, 'dashboard/board.html', context)
+        message = "Task created successfully"
+        messages.add_message(request, messages.INFO, message)
+        return redirect('dashboard:board', team_name=team_name, board_name=board_name)
     else:
         return Http404('Not allowed')
 
@@ -194,16 +156,8 @@ def move_task_right(request, team_name, board_name):
         else:
             message = "There is not columns to the right, the task can't be moved"
 
-        columns = selected_board.column_set.order_by('index')
-
-        context = {
-                'team': selected_team,
-                'board': selected_board,
-                'columns': columns,
-                'message': message
-                }
-
-        return render(request, 'dashboard/board.html', context)
+        messages.add_message(request, messages.INFO, message)
+        return redirect('dashboard:board', team_name=team_name, board_name=board_name)
     else:
         return Http404('Not allowed')
 
@@ -225,16 +179,8 @@ def move_task_left(request, team_name, board_name):
         else:
             message = "There is not columns to the left, the task can't be moved"
 
-        columns = selected_board.column_set.order_by('index')
-        
-        context = {
-                'team': selected_team,
-                'board': selected_board,
-                'columns': columns,
-                'message': message
-                }
-
-        return render(request, 'dashboard/board.html', context)
+        messages.add_message(request, messages.INFO, message)
+        return redirect('dashboard:board', team_name=team_name, board_name=board_name)
     else:
         return Http404('Not allowed')
 
@@ -248,18 +194,11 @@ def delete_task(request, team_name, board_name):
         selected_board = selected_team.board_set.get(name=board_name)
         selected_column = selected_board.column_set.get(name=request.POST.get('column_name', None))
         selected_task = selected_column.task_set.get(title=request.POST.get('task_title', None))
-        
         selected_task.delete()
         
-        columns = selected_board.column_set.order_by('index')
-
-        context = {
-                'team': selected_team,
-                'board': selected_board,
-                'columns': columns
-                }
-
-        return render(request, 'dashboard/board.html', context)
+        message = "Task deleted successfully"
+        messages.add_message(request, messages.INFO, message)
+        return redirect('dashboard:board', team_name=team_name, board_name=board_name)
     else:
         return Http404('Not allowed')
 
